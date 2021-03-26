30a177e9ff7174a1c79c4e9d7780828b9b679878
남승균 <skyun.nam@gmail.com>
2021-03-26 13:47:05 +0900

    Emaillist Fin

--- ServerEx/src/com/bit/servlet/dao/EmailDaoOrclImpl.java
+++ ServerEx/src/com/bit/servlet/dao/EmailDaoOrclImpl.java
@@ -2,6 +2,7 @@ package com.bit.servlet.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -51,7 +52,7 @@ public class EmailDaoOrclImpl implements EmailDao {
 				String email = rs.getString(4);
 				Date createAt = rs.getDate(5);	//	java.util.Date
 				
-				EmailVo vo = new EmailVo(no, firstName, lastName, email, createAt);
+				EmailVo vo = new EmailVo(no, lastName, firstName, email, createAt);
 				//	리스트에 추가
 				list.add(vo);
 			}
@@ -70,14 +71,61 @@ public class EmailDaoOrclImpl implements EmailDao {
 
 	@Override
 	public int insert(EmailVo vo) {
-		// TODO Auto-generated method stub
-		return 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int insertedCount = 0;
+		
+		try {
+			conn = getConnection();
+			String sql = "INSERT INTO emaillist" +
+					"(no, last_name, first_name, email) " +
+					"VALUES(seq_emaillist_pk.NEXTVAL, ?, ?, ?)";
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, vo.getLastName());
+			pstmt.setString(2, vo.getFirstName());
+			pstmt.setString(3, vo.getEmail());
+			
+			insertedCount = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				pstmt.close();
+				conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return insertedCount;
 	}
 
 	@Override
 	public int delete(Long no) {
-		// TODO Auto-generated method stub
-		return 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int deletedCount = 0;
+		
+		try {
+			conn = getConnection();
+			String sql = "DELETE FROM emaillist " +
+					"WHERE no = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setLong(1, no);
+			
+			//	쿼리 수행
+			deletedCount = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				pstmt.close();
+				conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return deletedCount;
 	}
 
 }
